1060282455a711f4f495bb52def143f951646f5f
FLLess <64878354+fllesser@users.noreply.github.com>
2024-11-28 23:35:38 +0800

    chore：b站解析下载视频路径调整

--- nonebot-plugin-resolver/handlers/bilibili.py
+++ nonebot-plugin-resolver/handlers/bilibili.py
@@ -170,7 +170,7 @@ async def bilibili_handler(bot: Bot, event: Event) -> None:
             streams = detecter.detect_best_streams()
             video_url, audio_url = streams[0].url, streams[1].url
             # 下载视频和音频
-            path = (rpath / "temp" / "video_id").absolute()
+            path = (rpath / "temp" / video_id).absolute()
             await asyncio.gather(
                     download_b_file(video_url, f"{path}-video.m4s", logger.info),
                     download_b_file(audio_url, f"{path}-audio.m4s", logger.info))
